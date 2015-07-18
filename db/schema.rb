0173bff76b653bebed2176908c4e9767a85ca2b9
Kyle Chapman <kylechapman09@gmail.com>
2015-07-18 18:28:18 -0500

    Link Reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718230647) do
+ActiveRecord::Schema.define(version: 20150718232206) do
 
 # Could not dump table "restaurants" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150718230647) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
